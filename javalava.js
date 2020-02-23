fd3cdbef8421caff5493398aae2086406ce04380
stasnam1 <stasnam6916@gmail.com>
2020-02-23 21:00:34 +1100

    counter loop added

--- javalava.js
+++ javalava.js
@@ -2,7 +2,12 @@ var counter=0;
 
 
 function displayValue(event){
-  counter = counter +1;  
+  counter = counter +1;
+  var textValue="";
+  for(i = 0; i < counter; i++){
+    textValue = textValue + '|';
+  }
+  document.getElementById("clicounter").innerHTML = textValue;  
   window.alert(document.getElementById('comment').value + " - exec times: " + counter);
 
 return false;
